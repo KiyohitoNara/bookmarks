33248b0509dda229fbc405f7a50544af04303c60
KiyohitoNara <kiyohitonara1216@gmail.com>
2023-05-07 16:05:42 +0900

    Added Folder enumeration

---
+++ BookmarksKit/Sources/Folder/Folder.swift
@@ -0,0 +1,11 @@
+//
+//  Folder.swift
+//
+//
+//  Copyright (c) 2023 Kiyohito Nara. All rights reserved.
+//
+
+public enum Folder: Int16, CaseIterable, Hashable {
+    case favorites = 0
+    case readingList = 1
+}
